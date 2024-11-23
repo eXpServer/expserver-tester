e090222478af15480d7b53bfa57b1c66a855e6d7
th3bossc <diljith2003@gmail.com>
2024-11-23 13:25:55 +0530

    removed accidental prisma upload

--- backend/prisma/migrations/20240929150054_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "File" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "filePath" TEXT NOT NULL,
-    "stageNo" INTEGER NOT NULL,
-    "userId" TEXT NOT NULL
-);
--- backend/prisma/migrations/20240929163219_made/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[stageNo]` on the table `File` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "File_stageNo_key" ON "File"("stageNo");
