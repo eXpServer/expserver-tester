9dd5d191b3523731baf4bf98e9efcdef89baf7e7
71203mayank <mayank71203@gmail.com>
2025-04-05 19:58:05 +0530

    minor changes

--- frontend/src/components/RamGraph/index.tsx
+++ frontend/src/components/RamGraph/index.tsx
@@ -4,10 +4,18 @@ const RamGraph = () => {
     return (
         <div className={styles['ram-graph']}>
             <div className={styles['ram-graph-vertical-scale']}>
-                <div className={styles['scale']}></div>
-                <div className={styles['scale']}></div>
-                <div className={styles['scale']}></div>
-                <div className={styles['scale']}></div>
+                <div className={styles['scale']}>
+                    <div className={'scale-value'}>100</div>
+                </div>
+                <div className={styles['scale']}>
+                    <div className={'scale-value'}>75</div>
+                </div>
+                <div className={styles['scale']}>
+                    <div className={'scale-value'}>50</div>
+                </div>
+                <div className={styles['scale']}>
+                    <div className={'scale-value'}>25</div>
+                </div>
             </div>
             <div className={styles['ram-graph-container']}>
 
